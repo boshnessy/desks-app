d265450b284398311f5c4144a9e3029c537a0314
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-07 16:22:24 -0600

    create all_desks route

--- app/controllers/desks_controller.rb
+++ app/controllers/desks_controller.rb
@@ -1,2 +1,12 @@
 class DesksController < ApplicationController
+  def desk_options
+    desks = Desk.all
+    desk_hashes = []
+
+    desks.each do |desk|
+      desk_hashes << {name: desk.name, price: desk.price, description: desk.description, image: desk.image_url}
+    end
+
+    render json: desk_hashes
+  end
 end
